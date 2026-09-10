c21613059210aaed5683c2930b24714a03e6a4f3
joao-arthur <joao.lothamer@gmail.com>
2026-09-10 07:18:14 -0300

    feat: risc-v linux hello world

---
+++ risc-v/hello_world.s
@@ -0,0 +1,18 @@
+.section .text
+.global _start
+
+_start:
+    li a0, 1
+    la a1, message
+    li a2, 13
+    li a7, 64
+    ecall
+
+    li a0, 0
+    li a7, 93
+    ecall
+
+.section .rodata
+
+message:
+    .ascii "Hello, World!\n"
